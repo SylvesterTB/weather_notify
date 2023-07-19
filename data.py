9be93e5792d514a0f179c1893fe207675dbd85f5
Sylvester <sylvester.broich@maibornwolff.de>
2023-07-19 16:35:51 +0200

    data.py func imported to main

--- data.py
+++ data.py
@@ -17,30 +17,26 @@ api_key = read_api_key()
 
 
 
-temp_alert = input('At what temperature would you like to be alerted (in celcius)?')
-temp_alert = int(temp_alert)
-city = "Munich" 
+#temp_alert = input('At what temperature would you like to be alerted (in celcius)?')
+#temp_alert = int(temp_alert)
+def weather_dect():
+    city = "Munich" 
 
-base_url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+    base_url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
 
-response = requests.get(base_url)
-data = response.json()
+    response = requests.get(base_url)
+    data = response.json()
 
-if response.status_code == 200:
-    temperature = data["current"]["temp_c"]
-    humidity = data["current"]["humidity"]
-    weather_description = data["current"]["condition"]["text"]
+    if response.status_code == 200:
+        temperature = data["current"]["temp_c"]
+        humidity = data["current"]["humidity"]
+        weather_description = data["current"]["condition"]["text"]
 
-    print(f"Weather in {city}:")
-    print(f"Temperature: {temperature} °C")
-    print(f"Humidity: {humidity}%")
-    print(f"Description: {weather_description}")
-else:
-    print("Error occurred while retrieving weather data.")
+    
 
-while temperature >= temp_alert:
-    if temperature >= temp_alert:
-        telebot('the temperature is greater than', temp_alert)
+    while temperature >= 20:
+        if temperature >= 20:
+            telebot('the temperature is greater than', 20)
     else:
         print("")
     time.sleep(1000)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+from data import *
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'The temperature in Munich' 
+    weather_dect() 
 
 app.run(host='0.0.0.0', port=81)
