d83b7cdc59866b5fd077cafa16f1b4a4827d92b0
Sylvester <sylvester.broich@maibornwolff.de>
2023-07-20 11:14:18 +0200

    Refactoring

--- data.py
+++ data.py
@@ -20,24 +20,28 @@ api_key = read_api_key()
 #temp_alert = input('At what temperature would you like to be alerted (in celcius)?')
 #temp_alert = int(temp_alert)
 
-def weather_detector():
-    city = "Munich" 
-
-    base_url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
-
-    message_frequency = 60 # in seconds
-    temperature_threshhold = 10
-
-    while temperature >= temperature_threshhold:
+def get_temperature():
+        city = "Munich" 
+        base_url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
         response = requests.get(base_url)
         data = response.json()
 
         if response.status_code == 200:
             temperature = data["current"]["temp_c"]
 
+        return temperature
+
+def weather_detector():
+    message_frequency = 60 # in seconds
+    temperature_threshhold = 10
+    temperature = get_temperature()
+
+    while temperature >= temperature_threshhold:
         if temperature >= temperature_threshhold:
             telebot('the temperature is greater than', temperature_threshhold)
         else:
             print("")
         time.sleep(message_frequency)
+        temperature = get_temperature()
+
 
